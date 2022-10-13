17cfb4e433b8ddc4cfef6c56e773b96134cade85
Yu Bin <subenmoom50@gmail.com>
2022-10-13 15:21:56 +0900

    ⚡:: 로그인, 회원가입 시 이메일 형식 검증 추가

--- src/main/java/com/example/ns/domain/user/present/dto/request/SignUpRequest.java
+++ src/main/java/com/example/ns/domain/user/present/dto/request/SignUpRequest.java
@@ -3,6 +3,7 @@ package com.example.ns.domain.user.present.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -12,6 +13,7 @@ public class SignUpRequest {
 
     @NotBlank(message = "email은 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
     @Size(min = 8, max = 50, message = "email은 8자 이상, 50자 이하여야 합니다.")
+    @Email
     private String email;
 
     @NotBlank(message = "password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
